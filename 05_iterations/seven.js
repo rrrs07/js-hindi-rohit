ef2bb13e5d2c49966e0926562c06dc94d5527e30
Rohit Sahu <116154715+rrrs07@users.noreply.github.com>
2024-02-11 10:39:19 +0000

    done

--- 05_iterations/seven.js
+++ 05_iterations/seven.js
@@ -1,4 +1,19 @@
 const myNums = [1,2,3,4,5,6,7,8,9,10]
 
-const newNums = myNums.map( (num) => num + 10)
+// const newNums = myNums.map( (num) => num + 10)
+// console.log(newNums);
+
+
+// myNums.forEach( (num) =>{
+//        console.log(num + 10);
+//      } )
+
+
+
+// channing
+const newNums = myNums
+                .map((num) => num * 10)
+                .map( (num) => num + 1)
+                .filter( (num) => num >=40)
+
 console.log(newNums);
